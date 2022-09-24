28bccc886f696a1856ff8a49739444b20b7e2114
LujanSolo <lujansolo@proton.me>
2022-09-24 12:48:08 -0700

    adjusted employee details text size via connected css. rendered the eng's github as a link

--- src/generateHTML.js
+++ src/generateHTML.js
@@ -1,13 +1,13 @@
 //build other specific functions for mgr, eng, int
 function genMgr(managers) {
   return `
-  <div class="card bg-light shadow-lg p-1 bg-white rounded" style="width: 18rem;">
+  <div class="card bg-light shadow-lg p-1 bg-white rounded mb-4" style="width: 18rem;">
   <div class="card-body bg-primary text-white rounded-2">
     <h4 class="card-title">${managers[0].name}</h4>
-    <h5 class="card-text"><i class="fa-solid fa-mug-hot"></i>Manager</h5>
+    <h5 class="card-text"><i class="fa-solid fa-mug-hot me-2"></i>Manager</h5>
   </div>
   <div class="m-3 border border-info">
-  <ul class="list-group list-group-flush">
+  <ul id="details" class="list-group list-group-flush">
     <li class="list-group-item">ID: ${managers[0].id}</li>
     <li class="list-group-item">Email: <a href="mailto:${managers[0].email}">${managers[0].email}</a></li>
     <li class="list-group-item">Office number: ${managers[0].officeNumber}</li>
@@ -25,16 +25,16 @@ function genEngineer(engineers) {
     .map(
       (member) =>
         `
-  <div class="card bg-light shadow-lg p-1 bg-white rounded" style="width: 18rem;">
+  <div class="card bg-light shadow-lg p-1 bg-white rounded mb-4" style="width: 18rem;">
   <div class="card-body bg-primary text-white rounded-2">
-    <h5 class="card-title"></i>${member.name}</h5>
-    <p class="card-text"><i class="fa-solid fa-glasses-round"></i>Engineer</p>
+    <h4 class="card-title"></i>${member.name}</h4>
+    <h5 class="card-text text"><i class="fa-solid fa-glasses me-2"></i>Engineer</h5>
   </div>
   <div class="m-3 border border-info">
-  <ul class="list-group list-group-flush">
+  <ul id="details" class="list-group list-group-flush">
     <li class="list-group-item">ID: ${member.id}</li>
     <li class="list-group-item">Email: <a href="mailto:${member.email}">${member.email}</a></li>
-    <li class="list-group-item">GitHub: ${member.github}</li>
+    <li class="list-group-item">GitHub: <a href="https://github.com/${member.github}">${member.github}</a></li>
   </ul>
   </div>
   </div>
@@ -51,13 +51,13 @@ function genIntern(interns) {
     .map(
       (member) =>
         `
-  <div class="card bg-light shadow-lg p-1 bg-white rounded" style="width: 18rem;">
+  <div class="card bg-light shadow-lg p-1 bg-white rounded mb-4" style="width: 18rem;">
   <div class="card-body bg-primary text-white rounded-2">
-    <h5 class="card-title p-3 mb-2 bg-primary text-white">${member.name}</h5>
-    <p class="card-text"><i class="fa-solid fa-user-graduate"></i>Intern</p>
+    <h4 class="card-title">${member.name}</h4>
+    <h5 class="card-text"><i class="fa-solid fa-user-graduate me-2"></i>Intern</h5>
   </div>
   <div class="m-3 border border-info">
-  <ul class="list-group list-group-flush">
+  <ul id="details" class="list-group list-group-flush">
     <li class="list-group-item">ID: ${member.id}</li>
     <li class="list-group-item">Email: <a href="mailto:${member.email}">${member.email}</a></li>
     <li class="list-group-item">School: ${member.school}</li>
@@ -72,49 +72,45 @@ function genIntern(interns) {
 function genHTML(teamMembers) {
   return `
   <!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <!-- BOOTSTRAP 5.2 LINKED -->
-   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet"
-   integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
- <!-- GOOGLE FONTS LINKED -->
- <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
- <script src="https://kit.fontawesome.com/2d308f669c.js" crossorigin="anonymous"></script>
- <link href="https://fonts.googleapis.com/css2?family=Recursive:ital@0;1&display=swap" rel="stylesheet" />
- <link rel="stylesheet" href="style.css" />
-  <title>Team Members</title>
-</head>
-<body>
-  <header>
-    <h1 id="masthead" class="text-center text-white py-2">My Team</h1>
-  </header>
-<body class="bg-light">
-
-<div class="container">
-
-<div class="row">
-    <div class="col">
-      ${genMgr(teamMembers.filter((member) => member.getRole() === "Manager"))}
+  <html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- BOOTSTRAP 5.2 LINKED -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+    <!-- GOOGLE FONTS LINKED -->
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <script src="https://kit.fontawesome.com/2d308f669c.js" crossorigin="anonymous"></script>
+    <link href="https://fonts.googleapis.com/css2?family=Recursive:ital@0;1&display=swap" rel="stylesheet" />
+    <link rel="stylesheet" href="style.css" />
+    <title>Team Members</title>
+  </head>
+    <header>
+      <h1 id="masthead" class="text-center text-white py-4 mb-5">My Team</h1>
+    </header>
+  <body class="bg-light">
+    <div class="container">
+      <div class="row">
+        <div class="col">
+          ${genMgr(teamMembers.filter((member) => member.getRole() === "Manager"))}
+        </div>
+        <div class="col">
+          ${genEngineer(teamMembers.filter((member) => member.getRole() === "Engineer"))}
+        </div>
+        <div class="col">
+          ${genIntern(teamMembers.filter((member) => member.getRole() === "Intern"))}
+        </div>
+      </div>
     </div>
-    <div class="col">
-      ${genEngineer(teamMembers.filter((member) => member.getRole() === "Engineer"))}
-    </div>
-    <div class="col">
-      ${genIntern(teamMembers.filter((member) => member.getRole() === "Intern"))}
-    </div>
-  </div>
-</div>
-
-</body>
-</html>
+  </body>
+  </html>
   `;
 }
 
 module.exports = genHTML;
 
-//TODO: Style cards in their individual functions.
-//TODO: Style card placement in <body> of genHTML function
-//todo: fix icon issues with engineer
+//todo MAKE github username a link
+//todo CHANGE "exit" to "BUILD TEAM"
+//todo ADD validation for input fields to ensure user input matches expected format
